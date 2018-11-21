cba5a2203a3cd6a73a1066020ce425c2560bdbcf
Кирилл Штеффен <kirillshteffen@gmail.com>
2018-11-21 17:25:03 +0600

    Initial Commit

---
+++ SettingsLoader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SettingsLoader
+//
+//  Created by Кирилл Штеффен on 21/11/2018.
+//  Copyright © 2018 Кирилл Штеффен. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
